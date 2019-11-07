56d1122036c138bac1293daf0f09c337bbfa3bec
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-07 19:04:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,6 @@ def roll_call_dwarves(dwarf_array)# code an argument here
   dwarf_array.each_with_index do |dwarf, index|
     puts "#{index + 1}. #{dwarf}"
   end
-  # Your code here
 end
 
 def summon_captain_planet(calls)
@@ -16,7 +15,6 @@ p summon_captain_planet(["earth", "wind", "water", "fire"])
 
 def long_planeteer_calls(calls)
   calls.any? {|call| call.length > 4}
-  # Your code here
 end
 
 def find_the_cheese(food_array)
