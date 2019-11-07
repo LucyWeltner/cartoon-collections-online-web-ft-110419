db7aa42dc792dcc41c70885071cd0be4d81db8ee
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-07 19:02:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,8 +19,6 @@ def long_planeteer_calls(calls)
   # Your code here
 end
 
-p long_planeteer_calls(["earth", "wind", "water", "fire"])
-
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
